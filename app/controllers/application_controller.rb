bfa880ca8c25d3a6007f8bb84500677742b4c58b
EC2 Default User <ec2-user@ip-172-31-47-176.ec2.internal>
2022-05-18 03:15:35 +0000

    [Add]headerlink

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,17 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
     
     
+    
+    # user画面へ
+    def after_sign_in_path_for(resource)
+     about_path
+    end
+    
+    def after_sign_out_path_for(resource)
+     homes_top_path
+    end
+    
+    
    protected
 
    def configure_permitted_parameters
